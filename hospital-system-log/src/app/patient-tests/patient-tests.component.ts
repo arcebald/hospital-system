e50427ed0b6da0b09c987af46f02e0c40b666238
Simion Jardan <simionjardan@Simions-MacBook-Pro.local>
2022-02-28 11:21:16 -0600

    completed patient tests component with all functions

--- hospital-system-log/src/app/patient-tests/patient-tests.component.ts
+++ hospital-system-log/src/app/patient-tests/patient-tests.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ApiService } from '../services/api.service';
+
 
 @Component({
   selector: 'app-patient-tests',
@@ -6,10 +12,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./patient-tests.component.scss']
 })
 export class PatientTestsComponent implements OnInit {
+  myParam: any;
+  patientTests: any;
 
-  constructor() { }
+  displayedColumns: string[] = ['name', 'testDate', 'testTime', 'testResult'];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  constructor(private api : ApiService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+     this.myParam = this.route.snapshot.paramMap.get('id');
+     console.log(this.myParam);
+     this.getPatientTests(this.myParam);
+  }
+
+  getPatientTests(myParam: number){
+    this.api.getPatientTest(myParam)
+    .subscribe({
+      next:(res)=>{
+        this.patientTests = res;
+        this.dataSource = res;
+        console.log(res);
+      },
+      error:(err)=>{
+        alert("Error while fetching test records!");
+      }
+    })
+  }
+
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
 }
