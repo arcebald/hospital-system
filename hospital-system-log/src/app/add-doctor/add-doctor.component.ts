eb64fef16b4059d1ec1c65c2c875106265e6b1a7
Simion Jardan <simionjardan@Simions-MacBook-Pro.local>
2022-02-28 11:06:47 -0600

    added info to add doctor components ts file

--- hospital-system-log/src/app/add-doctor/add-doctor.component.ts
+++ hospital-system-log/src/app/add-doctor/add-doctor.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ApiService } from '../services/api.service';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-add-doctor',
@@ -7,9 +10,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddDoctorComponent implements OnInit {
 
-  constructor() { }
+  doctorForm!: FormGroup;
+  actionBtn: string = "Save";
+  constructor(private formBuilder: FormBuilder,
+    private api: ApiService,
+    @Inject(MAT_DIALOG_DATA) public editData: any,
+    private dialogRef: MatDialogRef<AddDoctorComponent>) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+      this.doctorForm = this.formBuilder.group({
+        doctorName: ['', Validators.required],
+        specialization: ['', Validators.required]
+      });
+      console.log(this.editData);
+  
+      if (this.editData) {
+        this.actionBtn = "Update";
+        this.doctorForm.controls['doctorName'].setValue(this.editData.doctorName);
+        this.doctorForm.controls['specialization'].setValue(this.editData.specialization);
+      }
+    }
+
+    addDoctor() {
+      if (!this.editData) {
+        if (this.doctorForm.valid) {
+          this.api.postDoctor(this.doctorForm.value)
+            .subscribe({
+              next: (res) => {
+                alert("Doctor added succesfully!")
+                this.doctorForm.reset();
+                this.dialogRef.close('save');
+              },
+              error: () => {
+                alert("Error while adding the doctor")
+              }
+            })
+        }
+      } else {
+        this.updateDoctor()
+  
+        }
+      }
+      updateDoctor(){
+      this.api.putDoctor(this.doctorForm.value, this.editData.id)
+      .subscribe({
+        next:(res)=>{
+          alert("Doctor updated Successfully");
+          this.doctorForm.reset();
+          this.dialogRef.close('update');
+        },
+        error:()=>{
+          alert("Error while updating the doctor record!");
+        }
+      })
+      
+    }
 
 }
