3031e877e57fa2c6f888bbdf4505216dabf11500
Simion Jardan <simionjardan@Simions-MacBook-Pro.local>
2022-02-28 13:06:34 -0600

    adding doctors detail component to route file

--- hospital-system-log/src/app/app-routing.module.ts
+++ hospital-system-log/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DoctorDetailsComponent } from './doctor-details/doctor-details.component';
 import { DoctorsComponent } from './doctors/doctors.component';
 import { ImageComponent } from './image/image.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
@@ -11,6 +12,7 @@ const routes: Routes = [
   {path: "patients", component: PatientsComponent},
   {path: "doctors", component: DoctorsComponent},
   {path: "patients/:id", component: PatientTestsComponent},
+  {path: "doctors/:id" , component: DoctorDetailsComponent},
   {path: '', redirectTo: '', pathMatch:'full'},
   {path: "**", component: PageNotFoundComponent},
   
